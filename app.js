c47bb65a14e56af1d13b42c72d769b39a0ba6b7f
Jack <105589257+Qautno166@users.noreply.github.com>
2023-04-12 21:00:30 +0200

    Count time down/ Random position of mole

--- app.js
+++ app.js
@@ -5,11 +5,39 @@ const score = document.querySelector(".score");
 const time = document.querySelector(".time");
 const mole = document.querySelector(".mole");
 
-setInterval(randomSquare, 1000);
-function randomSquare() {
+let clock = 60;
+const randomSquare = setInterval(function () {
   squares.forEach((square) => {
+    square.innerHTML = "";
     square.classList.remove("mole");
   });
-  let randomPosition = Math.trunc(Math.random() * 9);
-  console.log(randomPosition);
-}
+
+  let randomPosition = squares[Math.trunc(Math.random() * 9)];
+  console.log(squares[Math.trunc(Math.random() * 9)]);
+  // randomPosition.innerHTML = '<img src="mole.jpg" />';
+  randomPosition.classList.add("mole");
+  if (randomPosition.classList.contains("mole")) {
+    randomPosition.innerHTML = '<img src="mole.jpg" />';
+  }
+  if (clock <= 55) {
+    squares.innerHTML = "";
+    clearInterval(randomSquare);
+    randomPosition.innerHTML = "";
+  }
+}, 1000);
+randomSquare;
+//TIME LEFT FUNCTION
+
+console.log(clock);
+const countTimeDown = setInterval(function () {
+  clock = clock - 1;
+  console.log(clock);
+  time.innerHTML = `Time Left:${clock}s`;
+  if (clock <= 55) {
+    time.innerHTML = "Time's Up";
+    clearInterval(countTimeDown);
+  }
+}, 1000);
+
+// console.log(clock);
+countTimeDown;
