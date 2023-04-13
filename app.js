c466c31fc512eac62117d1338446dca73b4e4a90
Jack <105589257+Qautno166@users.noreply.github.com>
2023-04-13 18:40:12 +0000

    Final

--- app.js
+++ app.js
@@ -5,39 +5,60 @@ const score = document.querySelector(".score");
 const time = document.querySelector(".time");
 const mole = document.querySelector(".mole");
 
+let scores = 0;
 let clock = 60;
+
+// forEach always asking fof all which is inside
 const randomSquare = setInterval(function () {
   squares.forEach((square) => {
     square.innerHTML = "";
     square.classList.remove("mole");
   });
 
+  // Making the squares random
   let randomPosition = squares[Math.trunc(Math.random() * 9)];
-  console.log(squares[Math.trunc(Math.random() * 9)]);
-  // randomPosition.innerHTML = '<img src="mole.jpg" />';
+
+  //Moving Mole
+
   randomPosition.classList.add("mole");
   if (randomPosition.classList.contains("mole")) {
     randomPosition.innerHTML = '<img src="mole.jpg" />';
   }
   if (clock <= 55) {
-    squares.innerHTML = "";
+    //How much time in seconds
+    // squares.innerHTML = "";
     clearInterval(randomSquare);
     randomPosition.innerHTML = "";
   }
 }, 1000);
 randomSquare;
-//TIME LEFT FUNCTION
+
+//Time left
 
 console.log(clock);
 const countTimeDown = setInterval(function () {
   clock = clock - 1;
-  console.log(clock);
+  // console.log(clock);
   time.innerHTML = `Time Left:${clock}s`;
   if (clock <= 55) {
     time.innerHTML = "Time's Up";
-    clearInterval(countTimeDown);
+    squares.forEach((square) => {
+      square.innerHTML = "";
+      square.classList.remove("mole");
+      clearInterval(countTimeDown);
+      score.innerHTML = `Final Score:${scores}`;
+      // alert(`YOUR FINAL SCORE IS ${scores}`);
+    });
   }
 }, 1000);
 
-// console.log(clock);
-countTimeDown;
+//SCORE FUNCTION
+squares.forEach((square) => {
+  square.addEventListener("click", function () {
+    if (square.classList.contains("mole")) {
+      scores = Number(scores + 1);
+      score.innerHTML = `Score:${scores}`;
+      // console.log(scores);
+    }
+  });
+});
